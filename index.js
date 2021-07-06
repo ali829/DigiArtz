9b1f62823d470b6ce612447d9917c64ac6da2538
ottis <ottisdjebi@gmail.com>
2021-07-07 00:03:51 +0100

    install mongoos and conect to database

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+
+const database = require('./config/database');
+
+//////////////////////////////////database setup//////////////////////////////////
+//connection database 
+mongoose.connect(database.url , {useNewUrlParser: true, useUnifiedTopology: true})
+//get notified if we connect succefully or connect error
+const db = mongoose.connection;
+db.on('error' , console.error.bind(console , 'connection error'))
+db.once('open' , () => {
+    console.log('mongoose is connecting...');
+});
+////////////////////////////////////////////////////////////////////////////////
 
 //init application
 const app = express();
